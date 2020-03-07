f99d2feecf4fc6f77f22fd45fc4b0b18224bc0c9
jimianelli <jim.ianelli@gmail.com>
2020-03-07 13:31:54 -0800

    adding files back?

---
+++ examples/BSAI_bsre/m_ai_18_1.bat
@@ -0,0 +1,13 @@
+copy ai2018_0_9.dat rougheye1_exp_18.dat
+copy ai18_har_0_9.ctl bsai_re.ctl
+copy compweights_m_ai_0_9_final.ctl compweights1.ctl
+rougheye1_exp_18 -nox 
+ 
+copy rougheye1_exp_18.rep m_ai_18_1.rep
+copy rougheye1_exp_18.std m_ai_18_1.std
+copy rougheye1_exp_18.par m_ai_18_1.par
+copy rougheye1_exp_18.rdat m_ai_18_1.rdat
+copy rougheye1_exp_18.cor m_ai_18_1.cor
+copy reproj.dat reproj_m_ai_18_1.dat
+copy reproj_age10.dat reproj_age10_m_ai_18_1.dat
+
---
+++ examples/GOAdusky/dusky.tpl
@@ -0,0 +1,1 @@
+../../src/dusky.tpl
